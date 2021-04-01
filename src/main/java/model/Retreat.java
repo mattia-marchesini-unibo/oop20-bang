cfe92bf0711d0939988f0f891a746cbcf63376de
RYAN <ryan.perrina@studio.unibo.it>
2021-04-01 18:10:36 +0200

    aggiunta classi stragy carte

---
+++ src/main/java/model/Retreat.java
@@ -0,0 +1,17 @@
+package model;
+
+public class Retreat implements Effects {
+
+	private int retreat;
+	
+	public Retreat(int retreat) {
+		super();
+		this.retreat = retreat;
+	}
+	
+	@Override
+	public void setEffects() {
+		//vado a modificare l'attributo 
+				//del player riferito alla posizione del giocatore
+	}		
+}
---
+++ src/main/java/model/Strike.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Strike implements Effects {
+
+
+	@Override
+	public void setEffects() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
