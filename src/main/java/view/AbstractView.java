3e5ac5b36f9fe55fb136464da1754e2d9276d40d
HDavi <davide.merli5@studio.unibo.it>
2021-04-06 19:56:07 +0200

    Modified interface View and class AbstractView

--- src/main/java/view/AbstractView.java
+++ src/main/java/view/AbstractView.java
@@ -1,19 +1,33 @@
 package view;
 
-import java.awt.CardLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
 
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 import libs.observe.ObservableElement;
 
-public abstract class AbstractView {
+public abstract class AbstractView implements View {
     
+    private static final double PROPORTION = 1.5;
+    
+    protected JFrame frame;
     protected JPanel panel = new JPanel();
     private ObservableElement<String> changeScreenObservable;
     
-    //@Override
-    public JPanel getPanel() {
-        return this.panel;
+    public AbstractView(final JFrame frame) {
+        this.frame = frame;
+        if(!frame.isVisible()) {
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setLocationByPlatform(true);
+            Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+            frame.setSize((int) (dim.getWidth() / PROPORTION), (int) (dim.getHeight() / PROPORTION));
+        }
+        frame.getContentPane().removeAll();
+        frame.getContentPane().add(panel);
+        this.initView();
+        this.show();
     }
     
     //@Override
@@ -22,8 +36,10 @@ public abstract class AbstractView {
     }
 
     //@Override
-    public void show(final JPanel parentPanel, final CardLayout parentLayout, final String viewName) {
-        parentLayout.show(parentPanel, viewName);
+    public void show() {
+        this.frame.setVisible(true);
     }
+    
+    abstract public void initView();
 
 }
--- src/main/java/view/View.java
+++ src/main/java/view/View.java
@@ -1,16 +1,10 @@
 package view;
 
-import java.awt.CardLayout;
-
-import javax.swing.JPanel;
-
 import libs.observe.ObservableElement;
 
 public interface View {
     
-    JPanel getPanel();
-    
     ObservableElement<String> getChangeScreenObservable();
 
-    void show(final JPanel parentPanel, final CardLayout parentLayout, final String viewName);
+    void show();
 }
