61193655b12af8fbe141bd5050c1ccff54c3e0cf
RYAN <ryan.perrina@studio.unibo.it>
2021-04-06 15:53:36 +0200

    aggiunta dell'interfaccia Player

---
+++ src/main/java/model/Player.java
@@ -0,0 +1,47 @@
+package model;
+
+import java.util.List;
+
+public interface Player {
+	
+	/**
+	 * this method set the sight of the sight of the player
+	 * @param sight
+	 */
+	public void setRange(int sight);
+	
+	/**
+	 * this method set the index of the player
+	 * @param retreat
+	 */
+	public void setIndex(int retreat);
+	
+	/**
+	 * @return the sight
+	 */
+	public int getSight();
+	
+	/**
+	 * @return the index
+	 */
+	public int getIndex();
+	
+	/**
+	 * @return the hand of card of the player
+	 */
+	public List<Card> getCard();
+	
+	/**
+	 * this method is use to add a card in the hand of player
+	 * @param card
+	 */
+	public void addCard(Card card);
+	
+	/**
+	 * this method is use to use a card
+	 * @param card
+	 */
+	public void playCard(Card card);
+	
+	
+}
--- src/main/java/model/SimplePlayer.java
+++ src/main/java/model/SimplePlayer.java
@@ -2,7 +2,7 @@ package model;
 
 import java.util.List;
 
-public class SimplePlayer {
+public class SimplePlayer implements Player {
 
 	private static final int BASICAL_SIGHT = 1;
 	private int sight;
@@ -10,6 +10,7 @@ public class SimplePlayer {
 	private int pos;
 	private Character character;
 	private Role role;
+	private List<Card> hand;
 	
 	public SimplePlayer(int pos) {
 		this.pos = pos;
@@ -46,11 +47,29 @@ public class SimplePlayer {
 	}
 
 	public List<Card> getCard(){
-		
+		return this.hand;
 	}
 	
 	public void addCard(Card card) {
-		
+		hand.add(card);
 	}
 	
+	public void playCard(Card card) {
+		if(card.getLifeTime() == LifeTime.DEADLY) {
+			//chiamo l'effetto della carta
+			this.removeCard(card); 
+			
+		} else {//if the cart is deadness i only have to call the effects
+			//chiamo l'effetto della carta
+			
+		}
+	}
+	
+	private void removeCard(Card card) {
+		this.hand.forEach(i -> {
+			if(i.equals(card)) {
+				//allora rimuovo quella specifica carta
+			}
+		});
+	}
 }
