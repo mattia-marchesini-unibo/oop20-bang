94f727f570764b35d6e0c5a6ca6493e96f30bdb6
Mattia Marchesini <mattia.marchesini5@studio.unibo.it>
2021-04-14 16:12:43 +0200

    controllers map now is static

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -10,7 +10,23 @@ import model.GameStateMachine;
 public class Controller {
 
     private IViewFactory factory;
-    private Map<String, IViewController> controllers;
+
+    private static Map<String, IViewController> controllers = new HashMap<>(Map.ofEntries(
+        entry("start", (fct) -> {
+            IView v = fct.getStartView();
+            v.show();
+        }),
+        entry("newGame", (fct) -> {
+            IView v = fct.getNewGameView();
+            v.show();
+        }),
+        entry("game", (fct) -> {
+            IView v = fct.getGameView(players);
+//            GameStateMachine machine = new GameStateMachine(new Table());
+            v.show();
+        })
+    ));;
+
     ObservableElement<String> changeSceneObs = new ObservableElement<>();
 
 //    private ObservableElement<Integer> players = new ObservableElement<>();
@@ -18,29 +34,12 @@ public class Controller {
     public Controller(IViewFactory factory) {
         this.factory = factory;
         this.factory.setChangeSceneObservable(this.changeSceneObs);
-        
-
-        controllers = new HashMap<>(Map.ofEntries(
-            entry("start", (fct) -> {
-                IView v = fct.getStartView();
-                v.show();
-            }),
-            entry("newGame", (fct) -> {
-                IView v = fct.getNewGameView();
-                v.show();
-            }),
-            entry("game", (fct) -> {
-                IView v = fct.getGameView(players);
-//                GameStateMachine machine = new GameStateMachine(new Table());
-                v.show();
-            })
-        ));
 
         changeSceneObs.addObserver(() -> {
-            this.controllers.get(changeSceneObs.get()).setup(this.factory);
+            controllers.get(changeSceneObs.get()).setup(this.factory);
         });
     }
-    
+
     public void start() {
         this.changeSceneObs.set("start");
     }
