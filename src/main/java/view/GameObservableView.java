8c0615647328e44be7d21a7282bfe9d3130f6818
ryan.perrina <ryan.perrina@studio.unibo.it>
2021-04-15 15:41:18 +0200

    modifiche all' GAmeObservableView

--- src/main/java/view/GameObservableView.java
+++ src/main/java/view/GameObservableView.java
@@ -7,28 +7,25 @@ import model.Card;
 
 public class GameObservableView {
 
-	public ObservableElement<Character> character;
-	public ObservableElement<String> name;
-	public ObservableElement<Role> role;
+	public ObservableElement<String> character;
+	public ObservableElement<String> role;
 	public ObservableElement<Integer> lifePoints;
-	public ObservableElement<List<Card>> hand;
-	public ObservableElement<List<SimplePlayer>> otherPlayer;
-	public ObservableElement<Character> getCharacter() {
+	public ObservableElement<List<Integer>> hand;
+	public ObservableElement<List<Integer>> otherPlayer;
+	public ObservableElement<List<Integer>> otherLft;
+	public ObservableElement<List<List<Integer>>> otherBluCard;
+	public ObservableElement<List<Integer>> otherNumCard;
+	
+	public ObservableElement<String> getCharacter() {
 		return character;
 	}
-	public void setCharacter(ObservableElement<Character> character) {
+	public void setCharacter(ObservableElement<String> character) {
 		this.character = character;
 	}
-	public ObservableElement<String> getName() {
-		return name;
-	}
-	public void setName(ObservableElement<String> name) {
-		this.name = name;
-	}
-	public ObservableElement<Role> getRole() {
+	public ObservableElement<String> getRole() {
 		return role;
 	}
-	public void setRole(ObservableElement<Role> role) {
+	public void setRole(ObservableElement<String> role) {
 		this.role = role;
 	}
 	public ObservableElement<Integer> getLifePoints() {
@@ -37,17 +34,36 @@ public class GameObservableView {
 	public void setLifePoints(ObservableElement<Integer> lifePoints) {
 		this.lifePoints = lifePoints;
 	}
-	public ObservableElement<List<Card>> getHand() {
+	public ObservableElement<List<Integer>> getHand() {
 		return hand;
 	}
-	public void setHand(ObservableElement<List<Card>> hand) {
+	public void setHand(ObservableElement<List<Integer>> hand) {
 		this.hand = hand;
 	}
-	public ObservableElement<List<SimplePlayer>> getOtherPlayer() {
+	public ObservableElement<List<Integer>> getOtherPlayer() {
 		return otherPlayer;
 	}
-	public void setOtherPlayer(ObservableElement<List<SimplePlayer>> otherPlayer) {
+	public void setOtherPlayer(ObservableElement<List<Integer>> otherPlayer) {
 		this.otherPlayer = otherPlayer;
 	}
+	public ObservableElement<List<Integer>> getOtherLft() {
+		return otherLft;
+	}
+	public void setOtherLft(ObservableElement<List<Integer>> otherLft) {
+		this.otherLft = otherLft;
+	}
+	public ObservableElement<List<List<Integer>>> getOtherBluCard() {
+		return otherBluCard;
+	}
+	public void setOtherBluCard(ObservableElement<List<List<Integer>>> otherBluCard) {
+		this.otherBluCard = otherBluCard;
+	}
+	public ObservableElement<List<Integer>> getOtherNumCard() {
+		return otherNumCard;
+	}
+	public void setOtherNumCard(ObservableElement<List<Integer>> otherNumCard) {
+		this.otherNumCard = otherNumCard;
+	}
+	
 	
 }
