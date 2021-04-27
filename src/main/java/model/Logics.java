236a7534e85f61843b2c6a6eb2f76f208db5788b
ryan.perrina <ryan.perrina@studio.unibo.it>
2021-04-27 18:51:46 +0200

    aggiunta metodi getTargets canUseBang

--- src/main/java/model/Logics.java
+++ src/main/java/model/Logics.java
@@ -11,11 +11,18 @@ public class Logics {
 	private SimpleTable table;
 	private CircularList<Player> currentPlayers;
 	
+	private static final List<Role> totalRoles = List.of(
+			Role.SHERIFF,Role.RENEGADE,Role.OUTLAW,Role.OUTLAW,Role.DEPUTY,Role.OUTLAW,Role.OUTLAW);
+
 	public Logics(final SimpleTable table) {
 		this.table = table;
 		this.currentPlayers = this.table.getPlayers();
 	}
 
+	public List<Role> getRolesForPlayers(final int playerNumber){
+		return totalRoles.subList(0, playerNumber);
+	}
+	
 	public Set<Player> getTargets() {		
 	        Player currentPlayer = this.table.getCurrentPlayer();
 		Set<Player> targets = new HashSet<Player>();
@@ -40,6 +47,13 @@ public class Logics {
 			cur = this.currentPlayers.getPrevOf(cur);
 		}
 		return targets;
+	}
+	
+	public boolean canUseBang() {
+		if(!this.table.getCurrentPlayer().getActiveCardsByName("volcanic").isEmpty()) {
+			return true;
+		}
+		return this.table.getCurrentPlayer().getUSeBang() ? false : true;
 	}
 }
 	
