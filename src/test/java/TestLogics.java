f8a40f1a8157cbdf25fc7180aba194b8745705b1
ryan.perrina <ryan.perrina@studio.unibo.it>
2021-04-30 22:22:28 +0200

    completamneto classe TestLogics

--- src/test/java/TestLogics.java
+++ src/test/java/TestLogics.java
@@ -16,21 +16,31 @@ public class TestLogics {
          @Test
          public void testGetTarget() {
              CircularList<Player> list = new CircularList<Player>();
-             Player p1 = new SimplePlayer(Role.OUTLAW,"ryan");
-             Player p2 = new SimplePlayer(Role.RENEGADE,"davide");
-             Player p3 = new SimplePlayer(Role.OUTLAW, "mattia");
-             Player p4 = new  SimplePlayer(Role.SHERIFF, "giulia");
+             Player p1 = new SimplePlayer(Role.OUTLAW,"p1");
+             Player p2 = new SimplePlayer(Role.RENEGADE,"p2");
+             Player p3 = new SimplePlayer(Role.OUTLAW, "p3");
+             Player p4 = new  SimplePlayer(Role.SHERIFF, "p4");
              list.add(p4);
              list.add(p3);
              list.add(p2);
              list.add(p1);
+             
+             p4.setRange(1);
+             var table = new SimpleTable(null,list);
+             Logics l = new Logics(table);
+             var set1 = l.getTargets();
+             assertEquals(Set.of(p3,p1), set1);
+             
              p3.modifyRetreat(1);
              p4.setRange(2);
-             Logics l = new Logics(new SimpleTable(null,list));
-             var set = l.getTargets();
-             assertEquals(Set.of(p3,p1,p2), set);
-             //prima tutto con range 1 
-             //poi con retreat
+             var set2 = l.getTargets();
+             assertEquals(Set.of(p3,p1,p2), set2);
+             
+             p4.setRange(1);
+             p3.modifyRetreat(1);
+             p1.modifyRetreat(1);
+             var set3 = l.getTargets();
+             assertEquals(Set.of(), set3);
          }
          
 }
