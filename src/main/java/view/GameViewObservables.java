be7dcdaac917caa2c3c1dc31f2ad548999c9813b
HDavi <davide.merli5@studio.unibo.it>
2021-04-30 22:19:42 +0200

    Modify GameViewObservables

--- src/main/java/view/GameViewObservables.java
+++ src/main/java/view/GameViewObservables.java
@@ -6,81 +6,27 @@ import libs.observe.*;
 
 public class GameViewObservables {
 
-    private ObservableElement<String> currentPlayer = new ObservableElement<>();
-    private ObservableElement<String> role = new ObservableElement<>();
-    private ObservableElement<Integer> lifePoints = new ObservableElement<>();
-    private ObservableElement<List<String>> hand = new ObservableElement<>();
-    private ObservableElement<List<String>> blueCards = new ObservableElement<>();
-    private ObservableElement<List<String>> otherPlayers = new ObservableElement<>();
-    private ObservableElement<List<Integer>> otherLifePoints = new ObservableElement<>();
-    private ObservableElement<List<List<String>>> otherBlueCards = new ObservableElement<>();
-    private ObservableElement<String> action = new ObservableElement<>();
+    private ObservableElement<CurrentPlayerInfo> currentPlayer = new ObservableElement<>();
+    private ObservableElement<List<PlayerInfo>> otherPlayers = new ObservableElement<>();
     private ObservableElement<List<String>> targets = new ObservableElement<>();
+    private ObservableElement<String> action = new ObservableElement<>();
     private String chosenCard = "";
     private String chosenPlayer = "";
     
-    public ObservableElement<String> getCurrentPlayer() {
+    public ObservableElement<CurrentPlayerInfo> getCurrentPlayer(){
         return this.currentPlayer;
     }
     
-    public void setCurrentPlayer(final ObservableElement<String> currentPlayer) {
-        this.currentPlayer = currentPlayer;
+    public void setCurrentPlayer(final CurrentPlayerInfo currentPlayerInfo) {
+        this.currentPlayer.set(currentPlayerInfo);
     }
     
-    public ObservableElement<String> getRole() {
-        return this.role;
-    }
-    
-    public void setRole(final ObservableElement<String> role) {
-        this.role = role;
-    }
-    
-    public ObservableElement<Integer> getLifePoints() {
-        return this.lifePoints;
-    }
-    
-    public void setLifePoints(final ObservableElement<Integer> lifePoints) {
-        this.lifePoints = lifePoints;
-    }
-    
-    public ObservableElement<List<String>> getHand() {
-        return this.hand;
-    }
-    
-    public void setHand(final ObservableElement<List<String>> hand) {
-        this.hand = hand;
-    }
-    
-    public ObservableElement<List<String>> getBlueCards() {
-        return this.blueCards;
-    }
-    
-    public void setBlueCards(final ObservableElement<List<String>> blueCards) {
-        this.blueCards = blueCards;
-    }
-    
-    public ObservableElement<List<String>> getOtherPlayers() {
+    public ObservableElement<List<PlayerInfo>> getOtherPlayers(){
         return this.otherPlayers;
     }
     
-    public void setOtherPlayer(final ObservableElement<List<String>> otherPlayers) {
-        this.otherPlayers = otherPlayers;
-    }
-    
-    public ObservableElement<List<Integer>> getOtherLifePoints() {
-        return this.otherLifePoints;
-    }
-    
-    public void setOtherLifePoints(final ObservableElement<List<Integer>> otherLifePoints) {
-        this.otherLifePoints = otherLifePoints;
-    }
-    
-    public ObservableElement<List<List<String>>> getOtherBlueCards() {
-        return this.otherBlueCards;
-    }
-    
-    public void setOtherBlueCards(final ObservableElement<List<List<String>>> otherBlueCards) {
-        this.otherBlueCards = otherBlueCards;
+    public void setOtherPlayers(final List<PlayerInfo> otherPlayers) {
+        this.otherPlayers.set(otherPlayers);
     }
     
     public ObservableElement<String> getAction(){
