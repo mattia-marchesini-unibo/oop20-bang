1ca7fb4510f8397d0625891388d7f9d5c8663245
ryan.perrina <ryan.perrina@studio.unibo.it>
2021-05-02 21:56:24 +0200

    aggiunti test state

--- src/test/java/TestState.java
+++ src/test/java/TestState.java
@@ -1,13 +1,16 @@
 import org.junit.Test;
 
+import controller.GameController;
 import libs.CircularList;
 import model.GameStateMachine;
 import model.Player;
 import model.Role;
 import model.SimplePlayer;
 import model.SimpleTable;
+import model.Table;
 import model.card.Card;
 import model.card.Color;
+import model.deck.Deck;
 import model.states.CheckDeadPlayersState;
 import model.states.CheckGameOverState;
 import model.states.ChooseActionState;
@@ -17,126 +20,145 @@ import model.states.EndTurnState;
 import model.states.PlayCardState;
 import model.states.StartTurnState;
 
+import static java.util.Map.entry;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertTrue;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class TestState {
+	private final Table table = new SimpleTable(new Deck(), 4);
+	private final GameStateMachine gsMachine = new GameStateMachine(table);
+	private final Card card = new Card("17827",Color.BLUE.name(), "bang", "bang");
+	private final List<Player> winners = null;
+
+	 private Map<String, Runnable> gsMachineMessages = new HashMap<String, Runnable>(
+		        Map.ofEntries(entry("playCard", () -> {
+		            gsMachine.setCurrentState(new PlayCardState(card));
+		            gsMachine.go();
+		        }), entry("choosePlayer", () -> {
+		            Table table = this.gsMachine.getTable();
+		          //  List<String> l = table.getChosenPlayerList().stream().map(p -> getPlayerName(p)).collect(Collectors.toList());
+		            //this.gameObs.getTargets().set(l);
+		        }), entry("playedCard", () -> {
+		            //drawTable();
+		        }), entry("startTurn", () -> {
+		            //drawTable();
+		        }), entry("endTurn", () -> {
+		            //drawTable();
+		        }), entry("discardCard", () -> {
+		            Player current = gsMachine.getTable().getCurrentPlayer();
+		            //Card card = current.getCardsByName(gameObs.getChosenCard()).get(0);
+		            current.removeCard(card);
+		            //drawTable();
+		        }), entry("end", () -> {
+//		            this.winners.addAll(
+//		                gsMachine.getTable().getPlayers().stream().map(p -> getPlayerName(p)).collect(Collectors.toList()));
+//		            changeSceneObs.set("end");
+		        })));
 
 	@Test
 	public void testChooseActionState() {
-		CircularList<Player> c = new CircularList<Player>();
-		c.add(new SimplePlayer(Role.DEPUTY, "p1"));
-		GameStateMachine g = new GameStateMachine(new SimpleTable(null,c));
-		g.setCurrentState(new ChooseActionState());
-		g.setMessage("PlayCard");
-		g.go();
-		assertEquals(g.getCurrentState().getClass(), new ChoosePlayerCardState().getClass());
-		g.setCurrentState(new ChooseActionState());
-		g.setMessage("EndTurn");
-		g.go();
-		assertEquals(g.getCurrentState().getClass(), new EndTurnState().getClass());
+		var obs = this.gsMachine.getMessageObservable();
+		obs.addObserver(() -> {
+			if(this.gsMachineMessages.containsKey(obs.get())) {
+				this.gsMachineMessages.get(obs.get()).run();
+			}
+		});
+		this.gsMachine.setCurrentState(new ChooseActionState("playCard"));
+		this.gsMachine.go();
+		this.gsMachine.getTable().getChoosePlayerObservable().set(new SimplePlayer(Role.DEPUTY, "p"));
+		
+		assertEquals(this.gsMachine.getCurrentState().getClass(), new ChoosePlayerCardState().getClass());
 	}
-	 
+
 	@Test
 	public void testCheckDeadPlayersState() {
-		CircularList<Player> c = new CircularList<Player>();
-		Player p1 = new SimplePlayer(Role.DEPUTY, "p1");
-		c.add(p1);
-		GameStateMachine g = new GameStateMachine(new SimpleTable(null,c));
-		g.setCurrentState(new CheckDeadPlayersState());
-		g.setMessage("");
-		g.go();
-		assertEquals(g.getCurrentState().getClass(), new ChooseActionState().getClass());
-		p1.modifyLifePoints(-5);
-		g.setCurrentState(new CheckDeadPlayersState());
-		g.go();
-		assertEquals(g.getCurrentState().getClass(), new CheckGameOverState().getClass());
+		Player player = new SimplePlayer(Role.DEPUTY, "p");
+		var obs = this.gsMachine.getMessageObservable();
+		obs.addObserver(() -> {
+			if(this.gsMachineMessages.containsKey(obs.get())) {
+				this.gsMachineMessages.get(obs.get()).run();
+			}
+		});
+		this.gsMachine.setCurrentState(new ChooseActionState("playCard"));
+		this.gsMachine.go();
+		this.gsMachine.getTable().getChoosePlayerObservable().set(player);
+		assertEquals(this.gsMachine.getCurrentState().getClass(), new CheckDeadPlayersState().getClass());
 	}
-	
+
 	@Test
 	public void testCheckGameOverState() {
-		CircularList<Player> c = new CircularList<Player>();
-		Player p1 = new SimplePlayer(Role.SHERIFF, "p1");
-		c.add(p1);
-		Player p2 = new SimplePlayer(Role.OUTLAW, "p2");
-		c.add(p2);
-		GameStateMachine g = new GameStateMachine(new SimpleTable(null,c));
-		g.setCurrentState(new CheckGameOverState());
-		g.setMessage("");
-		g.go();
-		assertEquals(g.getCurrentState().getClass(), new ChooseActionState().getClass());
-		c.remove(p1);
-		g.setCurrentState(new CheckGameOverState());
-		g.go();
-		assertEquals(g.getCurrentState().getClass(), new EndGameState().getClass());
-		c.add(p1);
-		c.remove(p2);
-		g.setCurrentState(new CheckGameOverState());
-		g.go();
-		assertEquals(g.getCurrentState().getClass(), new EndGameState().getClass());
+		this.gsMachine.getTable().getPlayers().forEach(i -> {
+			if(i.getRole().equals(Role.OUTLAW)) {
+				i.modifyLifePoints(- i.getLifePoints());
+			}
+		});
+		this.gsMachine.setCurrentState(new CheckDeadPlayersState());
+		this.gsMachine.go();
+		assertEquals(this.gsMachine.getCurrentState().getClass(), new CheckGameOverState().getClass());
 	}
+
 	@Test
 	public void testEndTurnState() {
-		CircularList<Player> c = new CircularList<Player>();
-		Player p1 = new SimplePlayer(Role.SHERIFF, "p1");
-		c.add(p1);
-		Player p2 = new SimplePlayer(Role.OUTLAW, "p2");
-		c.add(p2);
-		GameStateMachine g = new GameStateMachine(new SimpleTable(null,c));
-		g.setCurrentState(new EndTurnState());
-		g.setMessage("");
-		g.go();
-		assertEquals(g.getCurrentState().getClass(), new StartTurnState().getClass());
-		assertEquals(p2,g.getTable().getCurrentPlayer());
+		var cur = this.gsMachine.getTable().getCurrentPlayer();
+		this.gsMachine.setCurrentState(new ChooseActionState("endTurn"));
+		this.gsMachine.go();
+		assertEquals(this.gsMachine.getCurrentState().getClass(), new StartTurnState().getClass());
+		assertNotSame(cur,  this.gsMachine.getTable().getCurrentPlayer());
 	}
-	//serve mattia
+
 	@Test
 	public void testStartTurnState() {
-		CircularList<Player> c = new CircularList<Player>();
-		Player p1 = new SimplePlayer(Role.SHERIFF, "p1");
-		c.add(p1);
-		Player p2 = new SimplePlayer(Role.OUTLAW, "p2");
-		c.add(p2);
-		GameStateMachine g = new GameStateMachine(new SimpleTable(null,c));
-		g.setCurrentState(new StartTurnState());
-		g.setMessage("");
-		g.go();
-		assertEquals(g.getCurrentState().getClass(), new ChooseActionState().getClass());
-	}
-	
-	@Test
-	public void testChoosePlayerCardState() {
-		CircularList<Player> c = new CircularList<Player>();
-		Player p1 = new SimplePlayer(Role.SHERIFF, "p1");
-		c.add(p1);
-		Player p2 = new SimplePlayer(Role.OUTLAW, "p2");
-		c.add(p2);
-		GameStateMachine g = new GameStateMachine(new SimpleTable(null,c));
-		g.setCurrentState(new ChoosePlayerCardState());
-		g.setMessage("");
-		var card = new Card("b1",Color.BROWN,"bang","bang");
-		p1.addCard(card);
-		p2.addCard(card);
-		g.getTable().getChooseCardsObservable().set(Map.of(card,p1));
-		g.go();
-		assertEquals(g.getCurrentState().getClass(), new PlayCardState(card).getClass());
+		var cur = this.gsMachine.getTable().getCurrentPlayer();
+		var num = this.gsMachine.getTable().getCurrentPlayer().getCards().size();
+		this.gsMachine.setCurrentState(new StartTurnState());
+		this.gsMachine.go();
+		assertEquals(this.gsMachine.getCurrentState().getClass(), new StartTurnState().getClass());
+		assertEquals(cur.getCards().size(), num + 2);
 		
+		var num2 = this.gsMachine.getTable().getCurrentPlayer().getCards().size();
+		cur.setPrison(true);
+		this.gsMachine.setCurrentState(new StartTurnState());
+		this.gsMachine.go();
+		assertEquals(this.gsMachine.getCurrentState().getClass(), new StartTurnState().getClass());
+		assertEquals(cur.getCards().size(), num2 );
+		var newCur = this.gsMachine.getTable().getCurrentPlayer();
+		assertNotSame(cur, newCur);
+		assertEquals(newCur.getCards().size(), newCur.getLifePoints() + 2);
+		}
+
+	private class SetTrue {
+		public boolean b = false;
+		public void setTrue() {
+			this.b = true;
+		}
 	}
 	@Test
 	public void testPlayCardState() {
-		
+		this.gsMachine.setCurrentState(new PlayCardState(card));
+		var hasCard = new SetTrue();
+		System.out.println("ciao"+hasCard.b);
+		gsMachine.getTable().getChoosePlayerObservable().addObserver(() -> {
+			hasCard.setTrue();
+			
+		});
+		this.gsMachine.go();
+		assertTrue(hasCard.b);
 	}
-	
+
 	@Test
 	public void testEndGameState() {
-		CircularList<Player> c = new CircularList<Player>();
-		Player p1 = new SimplePlayer(Role.DEPUTY, "p1");
-		c.add(p1);
-		GameStateMachine g = new GameStateMachine(new SimpleTable(null,c));
-		g.setCurrentState(new EndGameState());
-		g.setMessage("");
-		g.go();
-		assertEquals(g.getMessageObservable().get(),"end");
+	this.gsMachine.getTable().getPlayers().forEach(i -> {
+		if(i.getRole().equals(Role.SHERIFF)) {
+			i.modifyLifePoints(- i.getLifePoints());
+		}
+	});
+	this.gsMachine.setCurrentState(new CheckDeadPlayersState());
+	this.gsMachine.go();
+	assertEquals(this.gsMachine.getCurrentState().getClass(), new EndGameState().getClass());
 	}
 }
