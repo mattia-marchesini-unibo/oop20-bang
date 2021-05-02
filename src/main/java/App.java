1be4bb6354c244b1d85b62a272bf6ca38476e367
Mattia Marchesini <mattia.marchesini5@studio.unibo.it>
2021-05-02 11:40:20 +0200

    prova

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,6 +4,7 @@ import view.SwingViewFactory;
 public class App {
 
     public static void main(String[] args) {
+        System.out.println("ciao");
         Controller c = new Controller(new SwingViewFactory());
         c.start();
     }
